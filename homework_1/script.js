f249b26f6b7f888d9ad53dfd819f4cecd832c6c2
ilyaTATARENTSEV <117466522+tatarentsev@users.noreply.github.com>
2025-01-31 21:42:07 +0300

    Introducing JavaScript

---
+++ homework_1/script.js
@@ -0,0 +1,10 @@
+// Example #1
+let celsius = 32;
+let fahrenheit = (celsius * (9 / 5)) + 32;
+console.log(`Градусов в Цельсиях: ${celsius}
+Градусов в Фарингейтах: ${fahrenheit}`);
+
+// Example #2
+const name = "Ilya";
+const admin = name;
+console.log(admin);
