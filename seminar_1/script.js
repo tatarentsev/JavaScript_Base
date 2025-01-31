3f11c8bed59d3a44687d16d100667df16578be18
ilyaTATARENTSEV <117466522+tatarentsev@users.noreply.github.com>
2025-01-31 21:41:28 +0300

    Introducing JavaScript

---
+++ seminar_1/script.js
@@ -0,0 +1,50 @@
+// Example #1
+// console.log("Hello from script.js");
+
+// Example #2
+// let num = 123;
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// let b = 1 + 2 + 3;
+// console.log(b);
+
+// First comment
+/* Second comment */
+
+// Example #3
+// const c = 10;
+// const d = 2;
+// const sum = c + d;
+// const minus = c - d;
+// const multiplication = c * d;
+// const division = c / d;
+// console.log(sum, minus, multiplication, division);
+// const total = sum + minus + multiplication + division;
+// console.log(total); 
+
+// Example #4
+// const a = 1.5;
+// const b = 0.75;
+// const result = a + b;
+// console.log(result);
+
+// const c = -2;
+// const d = 2;
+// console.log(-c, -d);
+
+// Example #5 
+const userName = "Ilya";
+const userLastName = "Tatarentsev";
+console.log(`Your name: ${userName}
+Your last name: ${userLastName}`);
+const lang = "java";
+const script = "script";
+const result = lang + script;
+console.log(result);
+const hello = "hello";
+const world = "world";
+console.log(`${hello} ${world}`);
