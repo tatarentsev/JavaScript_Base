593b28659c1fa361cf9ffcb754fcb7d9e26bb35b
ilyaTATARENTSEV <117466522+tatarentsev@users.noreply.github.com>
2025-02-01 18:04:35 +0300

    JavaScript Basics

---
+++ seminar_2/script.js
@@ -0,0 +1,62 @@
+// Example #1
+// let userAge = prompt("Ваш возраст: ");
+// alert(`Вам ${userAge} лет`);
+// let userName = prompt("Введите ваше имя: ");
+// alert(`Добро пожаловать на сайт, ${userName}`);
+
+// Example #2
+// let a = 13;
+// let b = 5;
+// console.log(a % b);
+
+// let d;
+// console.log(d);
+
+// console.log('abc' * 3);
+
+// console.log(1 / 0);
+// console.log(-1 / 0);
+
+// console.log('2' * '3');
+
+// Example #3
+// let a = '2';
+// let b = '3';
+// console.log(a + b);
+
+// let firstNum = Number(prompt("Введите первое число: "));
+// let secondNum = Number(prompt("Введите второе число: "));
+
+// console.log(`Сумма чисел равна: ${firstNum + secondNum}`);
+// console.log(`Разность чисел равна: ${firstNum - secondNum}`);
+// console.log(`Произведение чисел равно: ${firstNum / secondNum}`);
+// console.log(`Частное чисел равно: ${firstNum * secondNum}`);
+// console.log(`Остаток от деления чисел равен: ${firstNum % secondNum}`);
+
+// Example #4
+// let userNum = Number(prompt("Введите число: "));
+// if (userNum == 5) {
+//     console.log("Число равно 5");
+// } else if (userNum < 5) {
+//     console.log("Число меньше 5");
+// } else {
+//     console.log("Число больше 5");
+// }
+
+// let firstUserNum = Number(prompt("Введите первое число: "));
+// let secondUserNum = Number(prompt("Введите второе число: "));
+
+// if (firstUserNum === secondUserNum) {
+//     console.log("Числа равны");
+// } else if (firstUserNum < secondUserNum) {
+//     console.log(`Наименьшее число: ${firstUserNum}`);
+// } else {
+//     console.log(`Наименьшее число: ${secondUserNum}`);
+// }
+
+let userNum = Number(prompt("Введите число: "));
+if (userNum > 0 && userNum < 15) {
+    console.log("Подходит");
+} else {
+    console.log("Не подходит");
+}
